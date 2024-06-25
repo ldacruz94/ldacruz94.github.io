faaeabf9cddfd4787754f1b7bef27c0df5f003e5
Lucas <lucaspavesi11@gmail.com>
2024-06-24 22:40:43 -0400

    Update reading page

--- app/reading/page.tsx
+++ app/reading/page.tsx
@@ -41,14 +41,11 @@ export default function ReadingPage() {
         <h1 className="font-medium text-2xl mb-8 tracking-tighter">reading 📚</h1>
         <p className="text-lg">
           I&apos;m a bookworm. I love reading all sorts of genres, but besides non-engineering books, 
-          I like to brush up on my knowledge and skills by reading books on data structures and algorithms,
-          machine learning, networking, and devops. As well as particular programming languages such as Java,
-          Python, C#, C++, JS, etc.
+          I like to read a lot of books on how to improve as a software engineer.
         </p>
         <p className="text-lg pt-5">
-          I currently own over 200+ books my Kindle, 
-          around 60 paperback/hard cover books and textbooks, 
-          and a little over 100 books on another digital platform.
+          I currently own over 300+ e-books (200+ on Kindle), and
+          around 60 paperback books and textbooks.
         </p>
         <p className="text-lg pt-5">Here&apos;s some of the books I&apos;ve read:</p>
 
