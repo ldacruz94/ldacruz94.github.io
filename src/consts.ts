fd58448d08ec31272abd20d48c7653c874702a00
Lucas <lucaspavesi11@gmail.com>
2024-09-27 20:29:12 -0400

    Additional links fixed

--- src/consts.ts
+++ src/consts.ts
@@ -2,5 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Lucas';
-export const SITE_BASE = 'ldacruz.github.io'
 export const SITE_DESCRIPTION = 'Welcome to my website!';
