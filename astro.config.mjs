2c1596ca13a52fa136445741fc3adb6aa61ed279
Lucas <lucaspavesi11@gmail.com>
2024-09-27 21:03:18 -0400

    Revert changes back for now

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://ldacruz94.github.io',
-    // base: '/ldacruz.github.io',
+    base: '/ldacruz.github.io',
     integrations: [mdx(), sitemap(), tailwind()],
 });
